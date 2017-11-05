fd8a630761dd43d8671c0801f33e02b21ad4863c
Aditya Sharma <adityas129@gmail.com>
2017-11-05 04:28:38 -0500

    addedd try catch logic

--- myoband.cpp
+++ myoband.cpp
@@ -5,13 +5,25 @@ using namespace std;
 
 int main(){
 
+
   ifstream ip("orientationEuler-1509784830 - 5.csv");
+
+  try
+ {
+   throw 0;
+ }
+ catch (int e)
+ {
+   cout << "An exception occurred. Exception Nr. " << 0 << '\n';
+ }
 
   if(!ip.is_open()) std::cout << "ERROR: File Open" << '\n';
 
   string timestamp;
   string roll;
   int size = 0;
+
+
 
 
 
@@ -22,12 +34,17 @@ int main(){
     getline(ip,roll,',');
 
 
-    cout << "Timestamp: "<<timestamp<< " "<<timestamp << '\n';
-    cout << "Pushups: "<<roll << '\n';
+    //cout << "Timestamp: "<<timestamp<< " "<<timestamp << '\n';
+  //  cout << "Pushups: "<<roll << '\n';
 
   }
-  char * timestamparray[size];
-  char * rollarray[size];
+  char ** timestamparray;
+  char ** rollarray;
+  for (int i = 0; i < size; i++){
+    rollarray[i] = &roll[i];
+
+    cout  <<  & rollarray[i];
+  }
 
 
   ip.close();
