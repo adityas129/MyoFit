8d87a3b0a07d8494df8a294308b827412c4fe6f9
Aditya Sharma <adityas129@gmail.com>
2017-11-05 08:15:49 -0500

    final touches

--- myov3.cpp
+++ myov3.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <fstream>
 
@@ -5,27 +6,62 @@ using namespace std;
 
 int main(){
 
-  ifstream ip("orientationEuler-1509784830 - 5.csv");
+  ifstream ip("orientationEuler-1509785084 - 3.csv");
+
 
   if(!ip.is_open()) std::cout << "ERROR: File Open" << '\n';
+  //ip.ignore ( std::numeric_limits<stream::ip>::max(), '\n' );
+
 
   string timestamp;
   string roll;
-  int i = 0;
-  int size;
-  char *array= new char[size];
+  string pitch;
+  string yaw;
+  int counter =0;
+  double temp = 0;
+  double prev_value=0;
 
-
+  getline(ip,timestamp,',');
+  getline(ip,roll,',');
+  getline(ip,pitch,',');
+  getline(ip,yaw,'\n');
 
   while(ip.good()){
 
-    getline(ip,timestamp,',');
-    getline(ip,roll,',');
-    array[i]= atof(getline(ip,roll,','));
-    i++;
-  }
+        getline(ip,timestamp,',');
+        getline(ip,roll,',');
+        getline(ip,pitch,',');
+        getline(ip,yaw,'\n');
 
 
 
-  ip.close();
+
+        //std::cout << "timestamp: "<<timestamp<< '\n';
+        //std::cout << "roll: "<<roll << '\n';
+        //std::cout << "pitch: "<<pitch << '\n';
+        //std::cout << "yaw: "<<yaw << '\n';
+        cout << "prev_value start"<< prev_value<< endl;
+        cout << "temp start"<< temp<<endl;
+        cout << "counter start" << counter<< endl;
+
+        temp = std::stod(roll);
+
+        if (prev_value >= 1.0){
+          if (temp < 1.0){
+            counter = counter + 1;
+          }
+
+          }
+          prev_value = temp;
+          cout << "prev_value end"<< prev_value<< endl;
+          cout << "temp end"<< temp<<endl;
+          cout << "counter end" << counter<< endl;
+
+
+
+
+}
+cout << "Number of pushups completed is " << counter << endl;
+ip.close();
+
 }
